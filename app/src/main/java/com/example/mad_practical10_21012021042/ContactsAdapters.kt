0c5f4725d1cee9ecf517184a5547ca6c7c5bec7c
krutikarathod17 <krutikarathod21@gnu.ac.in>
2023-11-04 14:11:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical10_21012021042/ContactsAdapters.kt
@@ -0,0 +1,11 @@
+package com.example.mad_practical10_21012021042
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactsAdapters : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts_adapters)
+    }
+}
